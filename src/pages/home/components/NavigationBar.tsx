557c678733cfeedca94d6b466d461b938e8ac08b
Ahmad-Shehzad <73789931+Ahmad-Shehzad@users.noreply.github.com>
2023-06-17 10:16:45 +0100

    Created basic home page with navigation (#1)

---
+++ src/pages/home/components/NavigationBar.tsx
@@ -0,0 +1,38 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from "../../about/About";
+import Contact from "../../contact/Contact";
+import Home from "../Home";
+import "./NavigationBar.css";
+
+function NavigationBar() {
+  return (
+    <HashRouter>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li>
+            <a className="navText" href="/Website">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="navText" href="/Website/#/about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="navText" href="/Website/#/contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default NavigationBar;
